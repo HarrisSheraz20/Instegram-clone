19a83188b20272ed9bbf4537b6b0f906ca9598cf
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-03-18 13:43:58 +0000

    added a sign up button

--- instergram-clone/src/App.js
+++ instergram-clone/src/App.js
@@ -2,14 +2,43 @@ import React, { useState, useEffect} from "react"
 import './App.css';
 import Post from "./Components/Post/Post.js"
 import {db} from "./Firebase"
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
   const [posts, setPosts]= useState([]);
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
+    
     db.collection("posts").onSnapshot(snapshot =>{
+      
       setPosts(snapshot.docs.map(doc => ({
         id: doc.id,
         post: doc.data()
@@ -17,13 +46,17 @@ function App() {
     })
   }, []);
 
+  const signUp = (event) =>{
+
+  }
+
   return (
     
     <div className="app">
       
       <Modal
         open={open}
-        onClose={handleClose}
+        onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
           <h2>I am a Modal</h2>
@@ -37,6 +70,8 @@ function App() {
             alt=""
         />
       </div>
+
+      <Button onClick={() =>  setOpen(true)}>Sign up</Button>
 
       <h1>hello world</h1>
 
