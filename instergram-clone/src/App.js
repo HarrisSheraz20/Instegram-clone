30ecc0f6bbdb5ff4e7563275772e3b6b3b8b005a
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-03-18 12:34:54 +0000

    looking good

--- instergram-clone/src/App.js
+++ instergram-clone/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from "react"
 import './App.css';
+import Post from "./Components/Post/Post.js"
+import {db} from "./Firebase"
 
 function App() {
+
+  const [posts, setPosts]= useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot =>{
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a Modal</h2>
+        </div>
+      </Modal>
+      
+      <div className="app__header">
+        <img
+            className="app__headerImage"
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt=""
+        />
+      </div>
+
+      <h1>hello world</h1>
+
+      {
+        posts.map(({id, post})=>(
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+      
     </div>
   );
 }
